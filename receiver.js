acefb37468bb9ed71b8ac190641bd419f4dc02d7
SaiPavan93 <35818002+SaiPavan93@users.noreply.github.com>
2018-08-10 12:54:29 -0400

    HTML,JS,Bootstrap,JQuery,PHP
    Locating the nearest Hospital or the blood donation center

---
+++ receiver.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    $("#y1").click(function(){
+        $("#t1").hide();
+    });
+    $("#y1").click(function(){
+        $("#t2").show();
+    });
+	$("#n1").click(function(){
+        $("#t1").hide();
+    });
+    $("#n1").click(function(){
+         $("#t2").show();
+    });
+	$("#m1").click(function(){
+        $("#t1").hide();
+    });
+    $("#m1").click(function(){
+         $("#t2").show();
+    });
+	$("#y2").click(function(){
+        $("#t2").hide();
+    });
+    $("#y2").click(function(){
+        $("#t4").show();
+    });
+	$("#n2").click(function(){
+        $("#t2").hide();
+    });
+    $("#n2").click(function(){
+         $("#t4").show();
+    });
+	$("#m2").click(function(){
+        $("#t2").hide();
+    });
+    $("#m2").click(function(){
+         $("#t4").show();
+    });
+	$("#y4").change(function(){
+        $("#t4").hide();
+    });
+    $("#y4").change(function(){
+        $("#t3").show();
+    });
+	$("#y4").change(function(){
+        $("#t5").show();
+    });
+});
+
+
+   
